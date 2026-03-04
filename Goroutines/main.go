f0aaf6327c82b28c9ecd66f3a2a7b8fd0a471d9e
Fahad-I-Khan <khanfahad2105@gmail.com>
2026-03-05 01:38:01 +0530

    Return ordered output using workers

--- Goroutines/main.go
+++ Goroutines/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // Return Unordered output using goroutines
 // func main() {
@@ -17,25 +20,80 @@ import "fmt"
 // }
 
 // Return Ordered output using goroutines
+// type Result struct {
+// 	Index int
+// 	Value int
+// }
+
+// func main() {
+// 	nums := []int{1, 2, 3, 4, 5}
+// 	ch := make(chan Result, len(nums))
+// 	for i, num := range nums {
+// 		go func (i, num int)  {
+// 			// ch <- Result{i,num}
+// 			ch <- Result{Index: i, Value: num}
+// 		}(i, num)
+// 	}
+
+// 	results := make([]int, len(nums))
+// 	for range nums {
+// 		res := <- ch
+// 		results[res.Index] = res.Value
+// 	}
+// 	fmt.Println(results)
+// }
+
+// Ordered output using worker pool
+
+type Job struct {
+	Index int
+	Value int
+}
 
 type Result struct {
 	Index int
 	Value int
 }
 
+func worker(jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		results <- Result{
+			Index: job.Index,
+			Value: job.Value,
+		}
+	}
+}
+
 func main() {
 	nums := []int{1, 2, 3, 4, 5}
-	ch := make(chan Result, len(nums))
-	for i, num := range nums {
-		go func (i, num int)  {
-			// ch <- Result{i,num}
-			ch <- Result{Index: i, Value: num}
-		}(i, num)
+	jobs := make(chan Job, len(nums))
+	resultch := make(chan Result, len(nums))
+
+	var wg sync.WaitGroup
+	workerCount := 3
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go worker(jobs, resultch, &wg)
 	}
 
+	// Send jobs to the channel
+	go func ()  {
+		for i, num := range nums {
+			jobs <- Job{Index: i, Value: num}
+		}
+		close(jobs)
+	}()
+
+	// Wait for all workers to finish
+	go func ()  {
+		wg.Wait()
+		close(resultch)
+	}()
+
 	results := make([]int, len(nums))
-	for range nums {
-		res := <- ch
+
+	for res := range resultch {
 		results[res.Index] = res.Value
 	}
 	fmt.Println(results)
